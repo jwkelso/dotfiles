5e68cab2caf59a95fd9d118b1b45b54f3e6e3ed3
James Kelso <james.w.kelso@gmail.com>
2013-01-15 10:11:17 -0500

    Got Hodges' latest changes.

--- script/setup.rb
+++ script/setup.rb
@@ -11,55 +11,58 @@ require 'fileutils'
 
 # Get the git settings
 def read_git_credentials
-
-  print "Enter your full name: "
-  @git_name = gets
-  @git_name.chomp!
-  
-  print "Enter your email address: "
-  @git_email = gets
-  @git_email.chomp!
-
+    
+    print "Enter your full name: "
+    @git_name = gets
+    @git_name.chomp!
+    
+    print "Enter your email address: "
+    @git_email = gets
+    @git_email.chomp!
+    
 end
 
 # Overwrite git settings
 def overwrite_git_settings
-  File.open('.gitconfig', 'r+') do |f|
-    out = ""
-    f.each do |line|
-      if line.include? "name = "
-        if @git_name.nil? || @git_name.empty?
-          out << line
-        else
-          out << "\tname = " + @git_name + "\n"
+    File.open('.gitconfig', 'r+') do |f|
+        out = ""
+        f.each do |line|
+            if line.include? "name = "
+                if @git_name.nil? || @git_name.empty?
+                    out << line
+                    else
+                    out << "\tname = " + @git_name + "\n"
+                end
+                elsif line.include? "email = "
+                if @git_email.nil? || @git_email.empty?
+                    out << line
+                    else
+                    out << "\temail = " + @git_email + "\n"
+                end
+                else
+                out << line
+            end
         end
-      elsif line.include? "email = "
-        if @git_email.nil? || @git_email.empty?
-          out << line
-        else
-          out << "\temail = " + @git_email + "\n"
-        end
-      else
-        out << line
-      end
+        f.pos = 0
+        f.print out
+        f.truncate(f.pos)
     end
-    f.pos = 0
-    f.print out
-    f.truncate(f.pos)
-  end
 end
 
-# Copy over dotfiles
+# Link the files
 # Don't forget to add new files here as necessary
-def copy_files
-  FileUtils.cp '.bash_profile', ENV['HOME']+'/.bash_profile'
-  FileUtils.cp '.gitconfig', ENV['HOME']+'/.gitconfig'
-  FileUtils.cp '.gitignore_global', ENV['HOME']+'/.gitignore_global'
-  FileUtils.cp_r 'bin', ENV['HOME']+'/', :remove_destination => true
-  FileUtils.cp '.lldbinit', ENV['HOME']+'/.lldbinit'
+def link_files
+    FileUtils.ln_s ENV['HOME']+'/.dotfiles/.bash_profile', ENV['HOME']+'/.bash_profile', :force => true
+    FileUtils.ln_s ENV['HOME']+'/.dotfiles/.gitconfig', ENV['HOME']+'/.gitconfig', :force => true
+    FileUtils.ln_s ENV['HOME']+'/.dotfiles/.gitignore_global', ENV['HOME']+'/.gitignore_global', :force => true
+    FileUtils.ln_s ENV['HOME']+'/.dotfiles/.lldbinit', ENV['HOME']+'/.lldbinit', :force => true
+    
+    # directories are a bitch...
+    FileUtils.remove_dir ENV['HOME']+'/bin', true
+    FileUtils.ln_s ENV['HOME']+'/.dotfiles/bin', ENV['HOME']+'/', :force => true
 end
 
 # do all the magic
 read_git_credentials
 overwrite_git_settings
-copy_files
+link_files
